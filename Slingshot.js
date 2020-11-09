55fd5df7191b84c7dda217cd4ff6f46761748b08
Aakanksha Jain <rajain1010@gmail.com>
2020-11-09 17:50:12 +0530

    vanishing pigs

--- Slingshot.js
+++ Slingshot.js
@@ -18,6 +18,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach() {
+        this.sling.bodyA = bird.body;
+    }
+
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
